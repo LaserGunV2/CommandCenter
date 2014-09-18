3dc54b4613be6b327c6bd6b71d1c1185c6026ef7
Pascal <pascal@unpar.ac.id>
2014-09-18 16:02:50 +0700

    Simplify prajurit image

--- CommandCenter/CommandCenter/View/MapDrawer.cs
+++ CommandCenter/CommandCenter/View/MapDrawer.cs
@@ -34,7 +34,7 @@ namespace CommandCenter.View
                     if (prajurit.assignedPushPin == null && prajurit.location != null)
                     {
                         prajurit.assignedPushPin = new Pushpin();
-                        Uri imgUri = new Uri("../../img/soldier.png", UriKind.Relative);
+                        Uri imgUri = new Uri("../../img/stand.png", UriKind.Relative);
                         BitmapImage imgSourceR = new BitmapImage(imgUri);
                         ImageBrush imgBrush = new ImageBrush() { ImageSource = imgSourceR };
                         prajurit.assignedPushPin.Background = new SolidColorBrush(Colors.Red);
@@ -71,6 +71,16 @@ namespace CommandCenter.View
                 if (prajurit.assignedPushPin == null && prajurit.location != null)
                 {
                     prajurit.assignedPushPin = new Pushpin();
+                    Uri imgUri = new Uri("../../img/stand.png", UriKind.Relative);
+                    BitmapImage imgSourceR = new BitmapImage(imgUri);
+                    ImageBrush imgBrush = new ImageBrush() { ImageSource = imgSourceR };
+                    prajurit.assignedPushPin.Background = new SolidColorBrush(Colors.Red);
+                    prajurit.assignedPushPin.Content = new Ellipse()
+                    {
+                        Fill = imgBrush,
+                        Height = 20,
+                        Width = 20,
+                    };
                     prajurit.assignedPushPin.Location = prajurit.location;
                     ToolTipService.SetToolTip(prajurit.assignedPushPin, prajurit.nama);
                     map.Children.Add(prajurit.assignedPushPin);
@@ -80,7 +90,7 @@ namespace CommandCenter.View
                 {
                     // Note: Bing Maps fix to force update. Hopefully it would work
                     prajurit.assignedPushPin.Location = new Location(prajurit.location);
-                    prajurit.assignedPushPin.Heading = prajurit.heading;
+                    prajurit.assignedPushPin.Heading = (180 + prajurit.heading) % 360;
                 }
                 // Refresh map, if map is ready.
                 if (map.ActualHeight > 0 && map.ActualWidth > 0)
