6abe29ba440faa3833e4e5b2fce3189c816f910a
Pascal Alfadian Nugroho <pascalalfadian@live.com>
2014-12-02 09:33:56 +0700

    Implement group saving (replay file incompatible with previous versions)

--- CommandCenter/CommandCenter/Controller/AbstractGameController.cs
+++ CommandCenter/CommandCenter/Controller/AbstractGameController.cs
@@ -70,9 +70,15 @@ namespace CommandCenter.Controller
 
         public virtual void startExercise()
         {
+            string prajuritGroups = "";
+            for (int i = 0; i < prajurits.Count; i++)
+            {
+                prajuritGroups += prajurits[i].group;
+            }
+
             this.state = State.EXERCISE;
             parent.mapDrawer.showEveryone();
-            recorder.record(null, EventsRecorder.START);
+            recorder.record(null, EventsRecorder.START + "/" + prajuritGroups);
             parent.writeLog(LogLevel.Info, "Permainan dimulai");
         }
 
@@ -215,8 +221,13 @@ namespace CommandCenter.Controller
                 }
                 else if (type.Equals("pantau/state"))
                 {
-                    if (inPacket.getParameter("state").Equals("START"))
+                    if (inPacket.getParameter("state").StartsWith("START"))
                     {
+                        string[] tokens = inPacket.getParameter("state").Split('/');
+                        for (int i = 0; i < tokens[1].Length; i++)
+                        {
+                            prajurits[i].group = "" + tokens[1][i];
+                        }
                         startExercise();
                     }
                     else if (inPacket.getParameter("state").Equals("STOP"))
